27d27ecae0eee23e7eb34d2cba995121f7dd85c7
Ankit <ankitsharma.as420@gmail.com>
2024-04-02 11:55:29 +0530

    update and delete in service implementation.

--- Task-Management-System/src/main/java/com/TaskManagement/Task/Management/System/TaskServiceImpl/TaskServiceImpl.java
+++ Task-Management-System/src/main/java/com/TaskManagement/Task/Management/System/TaskServiceImpl/TaskServiceImpl.java
@@ -3,7 +3,9 @@ package com.TaskManagement.Task.Management.System.TaskServiceImpl;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,42 +26,79 @@ public class TaskServiceImpl implements TaskService {
     @Override
     public String addTask(AddTask addTask) {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        try{
-        Timestamp startDate = new Timestamp(sdf.parse(addTask.getStartDate()).getTime());
-
-        Timestamp endDate = new Timestamp(sdf.parse(addTask.getEndDate()).getTime());
-        TaskInfo taskinfo = new TaskInfo(addTask.getTaskName(),addTask.getDescription(),startDate,endDate,addTask.getStatus(),addTask.getAsignTo());
-        taskRepository.save(taskinfo);
-        return "Task successfully Added";
-        } catch(Exception e){
+        try {
+            Timestamp startDate = new Timestamp(sdf.parse(addTask.getStartDate()).getTime());
+            Timestamp endDate = new Timestamp(sdf.parse(addTask.getEndDate()).getTime());
+            TaskInfo taskinfo = new TaskInfo(addTask.getTaskName(), addTask.getDescription(), startDate, endDate,
+                    addTask.getStatus(), addTask.getAsignTo());
+            taskRepository.save(taskinfo);
+            return "Task successfully Added";
+        } catch (Exception e) {
             return "Task addition failed";
         }
     }
 
-    // To get the specific task using specific id. 
+    // To get the specific task using specific id.
     @Override
     public List<ManagerTaskInfo> getManagerTask(Long managerId) {
-        List<ManagerTaskInfo> response = new ArrayList<>();
-       List<TaskInfo> taskInfos = taskRepository.findByAsignTo(managerId);
-       for (TaskInfo taskInfo : taskInfos) {
-            ManagerTaskInfo managerTaskInfo = new ManagerTaskInfo();
-            managerTaskInfo.setTaskName(taskInfo.getTaskName());
-            managerTaskInfo.setDescription(taskInfo.getDescription());
-            managerTaskInfo.setStartDate(taskInfo.getStartDate());
-            managerTaskInfo.setEndDate(taskInfo.getEndDate());
-            managerTaskInfo.setStatus(taskInfo.getStatus());
-            response.add(managerTaskInfo);
-       }
-       return response;
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+
+            List<ManagerTaskInfo> response = new ArrayList<>();
+            List<TaskInfo> taskInfos = taskRepository.findByAsignTo(managerId);
+            for (TaskInfo taskInfo : taskInfos) {
+                ManagerTaskInfo managerTaskInfo = new ManagerTaskInfo();
+                managerTaskInfo.setTaskName(taskInfo.getTaskName());
+                managerTaskInfo.setDescription(taskInfo.getDescription());
+                // to change date formate
+                managerTaskInfo.setStartDate(sdf.format(new Date(taskInfo.getStartDate().getTime())));
+                managerTaskInfo.setEndDate(sdf.format(new Date(taskInfo.getEndDate().getTime())));
+                managerTaskInfo.setStatus(taskInfo.getStatus());
+                response.add(managerTaskInfo);
+            }
+            return response;
+        } catch (Exception e) {
+            return null;
+        }
     }
 
-    
-    // To update details the specific task using id. 
+    // To update details the specific task using id.
     @Override
-    public String updateTask(Long taskId, AddTask task) {
-        
-        return "";
+    public String updateTask(Long taskId, AddTask updatedTask) {
+        Optional<TaskInfo> optional = taskRepository.findById(taskId);
+        if (!optional.isPresent()) {
+            return null;
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            // to convert client side date into Timestap format to add in database.
+            Timestamp startDate = new Timestamp(sdf.parse(updatedTask.getStartDate()).getTime());
+            Timestamp endDate = new Timestamp(sdf.parse(updatedTask.getEndDate()).getTime());
+
+            TaskInfo taskInfo = optional.get();
+            taskInfo.setTaskName(updatedTask.getTaskName());
+            taskInfo.setDescription(updatedTask.getDescription());
+            taskInfo.setStartDate(startDate);
+            taskInfo.setEndDate(endDate);
+            taskInfo.setStatus(updatedTask.getStatus());
+            taskInfo.setAsignTo(updatedTask.getAsignTo());
+
+            // to save in database
+            taskRepository.save(taskInfo);
+            return "details are successfully Updated";
+        } catch (Exception e) {
+            return "Error: details not added";
+        }
     }
-    
+
+    @Override
+    public boolean DeleteTask(Long taskId) {
+        Optional<TaskInfo> optionalTask = taskRepository.findById(taskId);
+        if (optionalTask.isPresent()) {
+            taskRepository.deleteById(taskId);
+            return true;
+        }
+        return false;
+    }
 
 }
