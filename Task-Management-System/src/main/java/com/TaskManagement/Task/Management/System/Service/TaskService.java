5a0d6695b73a24f2835e8c447793eb9eb7af9ff7
Ankit <ankitsharma.as420@gmail.com>
2024-04-02 11:53:11 +0530

    add delete method in task service

--- Task-Management-System/src/main/java/com/TaskManagement/Task/Management/System/Service/TaskService.java
+++ Task-Management-System/src/main/java/com/TaskManagement/Task/Management/System/Service/TaskService.java
@@ -9,4 +9,5 @@ public interface TaskService {
     public String addTask(AddTask addTask);
     public List<ManagerTaskInfo> getManagerTask(Long managerId);
     public String updateTask(Long taskId, AddTask task);
+    public boolean DeleteTask(Long taskId);
 }
